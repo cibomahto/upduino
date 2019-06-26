eec6c573225360c0b3212e8f9d8c5b8247a5f5b4
Matt Mets <matt@blinkinlabs.com>
2019-06-26 11:07:08 +0200

    Use new API endpoint

--- supersweet/upload.sh
+++ supersweet/upload.sh
@@ -7,7 +7,7 @@ REMOTE_FILENAME=fpga.bin
 
 echo Uploading to: ${HOST}
 
-curl -X POST http://${HOST}/delete/${REMOTE_FILENAME}
-curl -X POST --upload-file  ${FILENAME} http://${HOST}/upload/${REMOTE_FILENAME}
+curl -X POST http://${HOST}/files/delete/${REMOTE_FILENAME}
+curl -X POST --upload-file  ${FILENAME} http://${HOST}/files/upload/${REMOTE_FILENAME}
 sleep 2
 curl -X POST http://${HOST}/fpga/update
